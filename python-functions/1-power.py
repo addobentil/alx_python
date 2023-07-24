974fe5632cd75ce83bde69d900a637b5c2a03077
Addo Bentil <addobentil100@gmail.com>
2023-07-23 23:14:24 -1200

    task 1

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,6 +1,9 @@
-def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
-    return result
 
+def pow(a, b):
+    if b >= 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
+    else:
+        return 1 / pow(a, -b)
