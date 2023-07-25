1d913fac93baa858b53de62d0580dc8b6bed5fee
Addo Bentil <addobentil100@gmail.com>
2023-07-25 00:45:38 -1200

    task 1

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,21 @@
+import sys
+
+def print_arguments():
+    # Get the number of arguments
+    num_arguments = len(sys.argv) - 1
+
+    # Print the number of arguments
+    print(num_arguments, ":  argument(s)")
+
+    if num_arguments == 0:
+        # If no arguments were passed, print a period and a new line
+        print(num_arguments ,"arguments.", end="\n")
+    else:
+        # If at least one argument was passed, print each argument and its position
+        print(":", end="\n")
+        for i in range(1, num_arguments + 1):
+            print("{}: {}".format(i, sys.argv[i]), end="\n")
+
+
+if __name__ == "__main__":
+    print_arguments()
