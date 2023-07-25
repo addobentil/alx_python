0aa52b4c4f2aef2ef9bcfa6ad587229a0d1dab3e
Addo Bentil <addobentil100@gmail.com>
2023-07-25 00:16:27 -1200

    task 0

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+
+if __name__ == '__main__':
+    
+    from add_0 import add
+    
+    a = 1
+    b = 2
+
+# Calculate the result of the addition using the 'add' function
+result = add(a, b)
+
+# Display the result using string formatting
+print("{} + {} = {}".format(a, b, result))
