7f39d5a6ad16d5be8a1a4ffd3ea153a5f2923e4e
Addo Bentil <addobentil100@gmail.com>
2023-07-25 23:39:47 -1200

    print matrix

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,6 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()  # Move to the next line after printing a row
+
