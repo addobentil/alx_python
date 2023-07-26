9c4b7c74ecd38bbf596a951b1d5c25ce0e476383
Addo Bentil <addobentil100@gmail.com>
2023-07-25 23:50:49 -1200

    print matrix task

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+        for i, num in enumerate(row):
+            if i > 0:
+                print(" ", end="")
+            print("{:d}".format(num), end="")
         print()  # Move to the next line after printing a row
 
