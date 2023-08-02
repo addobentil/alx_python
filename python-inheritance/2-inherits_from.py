bc954deea3a89063fe9cc993b0274fd58c8b07a7
Addo Bentil <addobentil100@gmail.com>
2023-08-01 23:50:05 -1200

    inherits from

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,13 @@
+def inherits_from(obj, a_class):
+    """
+    Check if an object is an instance of a class that inherited (directly or indirectly) from the specified class.
+
+    Parameters:
+        obj: Any - The object to check.
+        a_class: type - The class to compare the object against.
+
+    Returns:
+        bool: True if the object is an instance of a class that inherited from the specified class;
+              otherwise, False.
+    """
+    return isinstance(obj, type) and issubclass(type(obj), a_class) and type(obj) != a_class
