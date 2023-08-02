f7f5a2555d44d69292090d75bc916c39b7b12562
Addo Bentil <addobentil100@gmail.com>
2023-08-01 23:52:07 -1200

    inherited class-checking function

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Defines an inherited class-checking function."""
+
 def inherits_from(obj, a_class):
     """
     Check if an object is an instance of a class that inherited (directly or indirectly) from the specified class.
