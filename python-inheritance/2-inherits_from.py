aad3320304f546dc9ea3e08d59f9db3e5e4424f7
Addo Bentil <addobentil100@gmail.com>
2023-08-01 23:54:57 -1200

    inherited class-checking function

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -13,4 +13,4 @@ def inherits_from(obj, a_class):
         bool: True if the object is an instance of a class that inherited from the specified class;
               otherwise, False.
     """
-    return isinstance(obj, type) and issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
